1a9cb24e7139b9962b2edd2d1ba5f8889086961b
Rohit Gupta <rohitgupta03081996@gmail.com>
2024-09-03 16:02:16 +0530

    complete signup UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
-import Header from "./components/Header";
+import Header from './components/Header';
 
 
 export default function App() {
